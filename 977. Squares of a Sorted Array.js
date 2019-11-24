a2084cc05783be3894137e283570abb43d4c8e6d
w0955524096 <37862850+w0955524096@users.noreply.github.com>
2019-11-24 21:22:42 +0800

    Add files via upload
    平方在sort

---
+++ 977. Squares of a Sorted Array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortedSquares = function(A) {
+    for(let i=0;i<A.length;++i)
+        A[i]*=A[i];
+    A.sort(function (a, b) {
+  return a - b
+});;
+    
+    return A;
+};
