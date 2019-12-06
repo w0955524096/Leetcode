9daa3ae31fada9e6eb57b2342ee752493bd68579
w0955524096 <37862850+w0955524096@users.noreply.github.com>
2019-12-06 09:47:18 +0800

    pair@gmail.com
    把val相等的i與index交換
    i--(防止交換到一樣的)
    index--
    array.pop()

---
+++ 27. Remove Element.js
@@ -0,0 +1,1 @@
+/** * @param {number[]} nums * @param {number} val * @return {number} */var removeElement = function(nums, val) {    let index=nums.length-1    for (let i=0;i<nums.length;i++)        if(nums[i]===val)        {            [nums[i],nums[index]]=[nums[index],nums[i]];            i--;            index--;            nums.pop();        }};
