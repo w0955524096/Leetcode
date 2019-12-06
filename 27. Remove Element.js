368fd13e552f1c4d33a1114851ef3a74624fcb06
w0955524096 <37862850+w0955524096@users.noreply.github.com>
2019-12-06 09:51:49 +0800

    Update 27. Remove Element.js
    不需要index了
    直接把換到最後一個pop()
    i直接跟nums.length-1交換

--- 27. Remove Element.js
+++ 27. Remove Element.js
@@ -1,1 +1,19 @@
-/** * @param {number[]} nums * @param {number} val * @return {number} */var removeElement = function(nums, val) {    let index=nums.length-1    for (let i=0;i<nums.length;i++)        if(nums[i]===val)        {            [nums[i],nums[index]]=[nums[index],nums[i]];            i--;            index--;            nums.pop();        }};
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+ 
+    for (let i=0;i<nums.length;i++)
+        if(nums[i]===val)
+            {
+            [nums[i],nums[nums.length-1]]=[nums[nums.length-1],nums[i]];    
+             i--;  
+            nums.pop();
+            }
+     
+   
+
+   
+};
