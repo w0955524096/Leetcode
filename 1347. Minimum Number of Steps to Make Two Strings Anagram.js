52aef43d6b5dcaf9098860ca60992406ab6183bd
w0955524096 <37862850+w0955524096@users.noreply.github.com>
2020-03-17 18:35:06 +0800

    Add files via upload

--- 1347. Minimum Number of Steps to Make Two Strings Anagram.js
+++ 1347. Minimum Number of Steps to Make Two Strings Anagram.js
@@ -3,29 +3,27 @@
  * @param {string} t
  * @return {number}
  */
-var minSteps = function(s, t) {
-    
-//建立a-z
-    let sub="abcdefghijklmnopqrstuvwxyz"
-    
-//建立array
-    let arr=new Array(26);
-    for(let i=0;i<26;i++)
-        arr[i]=0;
+const minSteps = function (s, t) {
 
-//s[i]出現次數++;t[i]出現次數--
-    for(let i=0;i<s.length;i++)
-    {
-    arr[sub.indexOf(s[i])]++;    
-    arr[sub.indexOf(t[i])]--; 
-    }  
-    
+//建立a-z
+    let sub = "abcdefghijklmnopqrstuvwxyz"
+
+//建立array
+    let arr = new Array(26).fill(0);
+
+//與s[i]相同的++
+    for (let i = 0; i < s.length; i++) {
+        arr[sub.indexOf(s[i])]++;
+        arr[sub.indexOf(t[i])]--;
+    }
+
 //加總
-    let out=0
-    for(let i=0;i<26;i++)        
-        if(arr[i]>0)
-        out+=arr[i]
-    
+    let out = 0;
+    for (let i = 0; i < 26; i++) {
+        if (arr[i] < 0)
+            out += Math.abs(arr[i])
+    }
+
     return out
-  
+
 };
