35b1e8de0aeb4e3a005b370463815402c3bf6af6
w0955524096 <37862850+w0955524096@users.noreply.github.com>
2020-04-07 00:35:57 +0800

    ptree30202@gmail.com
    題目:
    https://leetcode.com/problems/print-words-vertically/
    解法:
    >>先分割字串
    >>取最大長度的字
    >>再依序放入字串

---
+++ 1324. Print Words Vertically.js
@@ -0,0 +1,26 @@
+﻿/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var printVertically = function (s) {
+  let SplitS = s.split(' ')
+
+  SplitS.sort(function (a, b) { return b.length - a.length })
+
+  const InLen = SplitS[0].length
+
+  const OutLen = SplitS.length
+
+  const Out = new Array(InLen).fill('')
+
+  SplitS = s.split(' ')
+
+  for (let i = 0; i < InLen; i++) {
+    for (let j = 0; j < OutLen; j++) {
+      if (!SplitS[j][i]) { Out[i] += ' ' } else { Out[i] += SplitS[j][i] }
+    }
+    Out[i] = Out[i].replace(/([\s]*$)/g, '')
+  }
+
+  return Out
+}
