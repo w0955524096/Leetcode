286a880f5e5dc5d11a8b769b14a7e83ad2cf4812
w0955524096 <37862850+w0955524096@users.noreply.github.com>
2020-04-07 22:51:31 +0800

    ptree30202@gmail.com
    題目:
    https://leetcode.com/problems/multiply-strings/
    解法:
    >>進位最左為0要去掉
    >>num1,2等於0時直接回傳'0'

---
+++ 43. Multiply Strings.js
@@ -0,0 +1,24 @@
+﻿/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function (num1, num2) {
+  if (num1 === '0' || num2 === '0') { return '0' }
+
+  const out = new Array(num1.length + num2.length).fill(0)
+  for (let i = num1.length - 1; i >= 0; i--) {
+    for (let j = num2.length - 1; j >= 0; j--) {
+      out[i + j + 1] += num1[i] * num2[j]
+    }
+  }
+
+  for (let i = out.length - 1; i >= 0; i--) {
+    out[i - 1] += Math.floor(out[i] / 10)
+    out[i] %= 10
+  }
+  // 進位最左為0就去掉
+  if (out[0] === 0)out[0] = ''
+
+  return out.join('')
+}
