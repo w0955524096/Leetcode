06fa95b6274f330445e9d411f559adb120f4e221
w0955524096 <37862850+w0955524096@users.noreply.github.com>
2020-04-08 01:42:59 +0800

    ptree30202@gmail.com
    題目:
    https://leetcode.com/problems/toeplitz-matrix/
    解法:
    >>分成直排跟橫排
    >>只要自身與右下的元素不同就回傳false

---
+++ 766. Toeplitz Matrix.js
@@ -0,0 +1,28 @@
+﻿/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var isToeplitzMatrix = function (matrix) {
+  let add
+  const matrixFirstLen = matrix[0].length
+  const matrixLen = matrix.length
+  // 直排
+  for (let i = 0; i < matrixLen; i++) {
+    add = 0
+    while (add + i < matrixLen - 1 && add < matrixFirstLen - 1) {
+      if (matrix[add + i][add] !== matrix[add + i + 1][add + 1]) { return false }
+      add++
+    }
+  }
+
+  // 橫排
+  for (let i = 1; i < matrixFirstLen; i++) {
+    add = 0
+    while (add + i < matrixFirstLen - 1 && add < matrixLen - 1) {
+      if (matrix[add][add + i] !== matrix[add + 1][add + i + 1]) { return false }
+      add++
+    }
+  }
+
+  return true
+}
